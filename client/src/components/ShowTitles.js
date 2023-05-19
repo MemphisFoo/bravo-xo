3085d8a9037f2d98694325ae1c0515b786896f7d
Christian TDN <cjwtdn@gmail.com>
2023-05-19 12:56:40 -0600

    committing changes to ShowTitles and ShowUsers

--- client/src/components/ShowTitles.js
+++ client/src/components/ShowTitles.js
@@ -12,8 +12,8 @@ function handleClick(){
 }
 
   return (
-    <div>
-       <div  className="items-left">
+    <div className="container md">
+       <div  className="relative inline-flex">
                 <button onClick={handleClick} className="relative inline-flex items-left justify-left p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-white rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
                     <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-black bg-opacity-99 dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                         {show.title}
